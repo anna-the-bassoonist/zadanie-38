ef073e4cdca08fadfec95af62613c3fa78a71796
annafagocistka <anafagocistka@gmail.com>
2018-07-23 12:51:36 +0200

    wynik

---
+++ main.js
@@ -0,0 +1,18 @@
+function dzialaniaLiczbowe(parametr1,parametr2){
+    if(parametr1+parametr2>0){
+    console.log("Wynik dodawania wynosi" + " " +(parametr1+parametr2));
+        } else {
+            console.log('wynik jest ujemny');
+        }
+    if(parametr1-parametr2>0){
+            console.log("Wynik odejmowania wynosi" + " " + (parametr1-parametr2));
+            
+        }  else {
+            console.log('wynik jest ujemny');}
+    if(parametr1*parametr2>0){
+            console.log("Wynik mnożenia wynosi" + " " + (parametr1-parametr2));
+        } else {
+            console.log('wynik jest ujemny');
+}
+}
+dzialaniaLiczbowe(10,20);
